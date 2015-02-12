0616a67f790e6c2e5fac54db2c51150ad0db5889
James-Thorson <JamesT.esq@gmail.com>
2015-02-12 12:25:15 -0800

    Added example

---
+++ examples/Example--Convert_discard_LF_to_SS3_format.R
@@ -0,0 +1,28 @@
+
+# Load package
+devtools::install_github("nwfsc-assess/nwfscDiscards")
+library(nwfscDiscards) # This should reflect the latest formal release
+
+# Read data
+data( Simulated_example_Length_Freq ) # Simulated numbers (not proportions)
+LengthBins = seq(12, 66, by=2)
+
+# Loop across gears and states
+for(gI in 1:length(unique(LF[,'Gear']))){
+for(sI in 1:length(unique(LF[,'State']))){
+  State = unique(LF[,'State'])[sI]
+  Gear = unique(LF[,'Gear'])[gI]
+
+  # Restrict to single fleet
+  LF_sub = LF[which(LF[,'Gear']==Gear & LF[,'State']==State),]
+  SampSize_sub = SampSize[which(SampSize[,'Gear']==Gear & SampSize[,'State']==State),]
+
+  # Length_Frequency_Table=LF; Sample_Size_Table=SampSize; LengthBins=LengthBins; season=1; fleet=NA; gender=0; partition=1
+  SS3 = Convert_discardLF_to_ss3_format_Fn( Length_Frequency_Table=LF_sub, LengthBins=LengthBins, season=1, fleet=NA, gender=0, partition=1 )
+
+  # Sample sizes
+  SS3[,'Nsamp'] =   # Add your own code for input sample sizes here
+  
+  # save as CSV
+  write.csv( file=paste0("Discard_comps_SS3format_State=",State,"_Gear=",Gear,".csv"), row.names=FALSE)
+}}
